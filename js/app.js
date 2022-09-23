1dd366163ea52f57f9965f808720af0d2c8fc05a
Alexander <mataale1790@gmail.com>
2022-09-23 13:54:41 -0600

    JavaScript Scroll Up

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const scrollup = document.querySelector("#scroll-up");
+scrollup.addEventListener("click", ()=>{
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+});
