98e3225e9c814ec9eb2c4258fd5dbed226fa05aa
Alexander <mataale1790@gmail.com>
2022-09-23 18:46:24 -0600

    Section Responsive About ME

--- js/app.js
+++ js/app.js
@@ -6,3 +6,18 @@ scrollup.addEventListener("click", ()=>{
         behavior: "smooth",
     });
 });
+
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("header nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", ()=> {
+    ul.classList.toggle("show");
+});
+
+const navLink = document.querySelectorAll(".nav-link");
+navLink.forEach((link)=>
+    link.addEventListener("click", () =>{
+        ul.classList.remove("show");
+    })
+);
